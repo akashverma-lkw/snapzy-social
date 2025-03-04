40e0629997137edbd758640a208663e9b120044f
Akash Verma <akashvermastp7@gmail.com>
2025-03-04 11:17:54 +0530

    update package and vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000, // ✅ Frontend will run on port 3000
-		proxy: {
-			"/api": {
-				target: import.meta.env.VITE_API_URL, // ✅ Correct way to access VITE env variables
-				changeOrigin: true,
-				secure: true, // 🔥 Secure https request
-				ws: true,
+export default defineConfig(({ mode }) => {
+	const API_URL = process.env.VITE_API_URL; // ✅ Environment variable yahan access karo
+
+	return {
+		plugins: [react()],
+		server: {
+			port: 3000,
+			proxy: {
+				"/api": {
+					target: API_URL, // ✅ Use the API URL
+					changeOrigin: true,
+					secure: true,
+					ws: true,
+				},
 			},
 		},
-	},
+	};
 });
