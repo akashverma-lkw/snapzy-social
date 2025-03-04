b9b6b99e0e4eefadbe7eaab69dc22dff150263c2
Akash Verma <akashvermastp7@gmail.com>
2025-03-04 11:10:11 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
 		port: 3000, // ✅ Ensures frontend runs on port 3000
 		proxy: {
 			"/api": {
-				target: "https://snapzy-backend.onrender.com", // ✅ Ensure this matches your backend port
+				target: import.meta.env.VITE_API_URL, // ✅ Ensure this matches your backend port
 				changeOrigin: true,
-				secure: false, // Optional: use `false` if you have HTTPS issues in development
+				secure: ture, // Optional: use `false` if you have HTTPS issues in development
 				ws: true, // Optional: handles WebSocket connections if needed
 			},
 		},
