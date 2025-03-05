0eaca5819d611a1e3fb665ed4b9cae1fbc40d190
Akash Verma <akashvermastp7@gmail.com>
2025-03-05 12:37:38 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 app.set("trust proxy", 1);
 
 // Middlewares (in correct order)
-app.use(cookieParser());
+
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -46,6 +46,8 @@ app.use(
     ],
   })
 );
+
+app.use(cookieParser());
 
 // Cloudinary Configuration
 cloudinary.config({
