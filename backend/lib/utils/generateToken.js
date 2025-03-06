6237eb3d69b5573708b98d61475f7ae976d54449
Akash Verma <akashvermastp7@gmail.com>
2025-03-06 14:53:17 +0530

    update

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const generateTokenAndSetCookie = (res, id) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+export const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '15d', // Token expiry time
   });
 
   res.cookie('jwt', token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true, // Prevent client-side access
     secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: 'none', // CSRF protection
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
   });
+
+  res.token = token;
 };
