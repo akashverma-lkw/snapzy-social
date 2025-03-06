dcf5f6a53077eda770fc5256244691b2f3ec3364
Akash Verma <akashvermastp7@gmail.com>
2025-03-06 13:55:09 +0530

    update

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,8 +5,8 @@ import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
 router.get("/me", protectRoute, getMe);
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", protectRoute, signup);
+router.post("/login", protectRoute, login);
 router.post("/logout", logout);
 
 export default router;
