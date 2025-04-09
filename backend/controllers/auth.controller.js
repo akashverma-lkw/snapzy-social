5ba29f0fe3fd58ee643a87b45f8d8a3890551901
Akash Verma <akashvermastp7@gmail.com>
2025-04-09 22:55:46 +0530

    revert auth.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
-		
+		generateTokenAndSetCookie(user._id, res);
 
 		res.status(200).json({
 			token: res.token,
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
 			profileImg: user.profileImg,
 			coverImg: user.coverImg,
 		});
-		generateTokenAndSetCookie(user._id, res);
+		
 	} catch (error) {
 		console.log("Error in login controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
